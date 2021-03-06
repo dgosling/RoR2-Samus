86e744419410fc7ac2f16026c52812ceb1c9ae67
dgosling <daniellgosling@gmail.com>
2021-03-06 16:54:25 -0700

    Morph ball physics is hard
    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ SamusMod/Misc/colision test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using RoR2;
+using System.Runtime.CompilerServices;
+
+namespace SamusMod.Misc
+{
+    public class colision_test : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision collision)
+        {
+            Debug.Log("collision started with "+collision.collider.name);
+        }
+
+        private void OnCollisionExit(Collision collision)
+        {
+            Debug.Log("collision stopped with "+collision.collider.name);
+        }
+    }
+}
